c765bf1a0eae42221f2ca523a33c56d65638365c
kwonjonny <128343377+kwonjonny@users.noreply.github.com>
2023-07-20 12:34:01 +0900

    version up commit
    version up commit

--- file/src/test/java/board/file/mappers/MemberMapperTests.java
+++ file/src/test/java/board/file/mappers/MemberMapperTests.java
@@ -16,7 +16,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @SpringBootTest
 public class MemberMapperTests {
-    
+
     @Autowired(required = false)
     private MemberMapper memberMapper;
 
@@ -27,77 +27,84 @@ public class MemberMapperTests {
     private static final String TEST_PASSWORD = "1weifdlsv";
     private static final String TEST_MEMBER_NAME = "권성준";
 
-    // BeforeEach 사용을 위한 MemberConvertDTO 정의 
+    // BeforeEach 사용을 위한 MemberConvertDTO 정의
     private MemberConvertDTO memberConvertDTO;
 
     // MemberService Create Member Test Set Up
     @BeforeEach
     public void setUp() {
         memberConvertDTO = MemberConvertDTO.builder()
-        .email(TEST_EMAIL)
-        .mpw(passwordEncoder.encode(TEST_PASSWORD))
-        .mname(TEST_MEMBER_NAME)
-        .build();
+                .email(TEST_EMAIL)
+                .mpw(passwordEncoder.encode(TEST_PASSWORD))
+                .mname(TEST_MEMBER_NAME)
+                .build();
     }
-    
 
-    // Join Member Mapper Test 
+    // Join Member Mapper Test
     @Test
     @Transactional
     @DisplayName("회원 가입 테스트")
     public void joinMemberTest() {
-        // GIVEN 
+        log.info("=== Start Join Member Mapper Test ===");
+        // GIVEN
         String roleName = "USER";
-        // WHEN 
+        // WHEN
         memberMapper.joinMember(memberConvertDTO);
         memberMapper.createJoinMemberRole(memberConvertDTO.getEmail(), roleName);
-        // THEN 
+        // THEN
+        log.info("=== End Join Member Mapper Test ===");
     }
 
-    // Update Member Mapper Test 
+    // Update Member Mapper Test
     @Test
     @Transactional
     @DisplayName("회원 업데이트 테스트")
     public void updateMemberTest() {
-        // GIVEN 
+        log.info("Start Update Member Mapper Test ===");
+        // GIVEN
         MemberConvertDTO memberConvertDTO = MemberConvertDTO.builder()
-        .email("thistrik@naver.com")
-        .mpw(passwordEncoder.encode("1111"))
-        .mname("김선제")
-        .build();
-        // WHEN 
+                .email("thistrik@naver.com")
+                .mpw(passwordEncoder.encode("1111"))
+                .mname("김선제")
+                .build();
+        // WHEN
         memberMapper.updateMember(memberConvertDTO);
-        // THEN 
+        // THEN
         memberMapper.selectMember("thistrik@naver.com");
         Assertions.assertNotNull("thistrik@naver.com", "Should Be Not Null");
+        log.info("End Update Member Mapper Test ===");
     }
 
-    // Delete Member Mapper Test 
+    // Delete Member Mapper Test
     @Test
     @Transactional
     @DisplayName("회원 삭제 테스트")
     public void deleteMemberTest() {
+        log.info("Start Delete Member Mapper Test ===");
         // GIVEN
         String email = "thistrik@naver.com";
-        // WHEN 
+        // WHEN
         memberMapper.deleteMember(email);
         memberMapper.deleteMemberRole(email);
-        // THEN 
+        // THEN
         MemberConvertDTO readMember = memberMapper.selectMember(email);
         Assertions.assertNull(readMember, "readMember Should Be Null");
+        log.info("End Delete Member Mapper Test ===");
     }
 
-    // Select Member Mapper Test 
+    // Select Member Mapper Test
     @Test
-    @Transactional(readOnly = true)
+    @Transactional
     @DisplayName("회원 조회 테스트")
     public void selectMemberTest() {
+        log.info("Start Read Member Mapper Test ===");
         // GIVEN
         String email = "thistrik@naver.com";
-        // WHEN 
+        // WHEN
         MemberConvertDTO readMember = memberMapper.selectMember(email);
-        // THEN 
+        // THEN
         log.info(readMember);
         Assertions.assertNotNull(readMember, "readMember Should Not Null");
+        log.info("End Read Member Mapper Test ===");
     }
 }
